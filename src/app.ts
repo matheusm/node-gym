0eef51691f6545e94899830c391a6d2c74ab407f
Matheus Martins <matheusms280100@gmail.com>
2024-05-27 23:10:54 -0300

    Update eslint configuration and add Prisma client

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,27 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 export const app = fastify()
+
+const prisma = new PrismaClient()
+
+async function main() {
+  await prisma.user.create({
+    data: {
+      name: 'Rich',
+      email: 'hello@prisma.com',
+    },
+  })
+
+  const allUsers = await prisma.user.findMany()
+  console.dir(allUsers, { depth: null })
+}
+
+main()
+  .catch(async (e) => {
+    console.error(e)
+    process.exit(1)
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
