4571e4bc66213038fc68253278add7a05e4a28cf
Matheus Martins <matheusms280100@gmail.com>
2024-06-03 21:16:06 -0300

    Add UserAlreadyExistsError and handle it in register function

--- src/http/register.ts
+++ src/http/register.ts
@@ -2,6 +2,7 @@ import { FastifyRequest, FastifyReply } from 'fastify'
 import { z } from 'zod'
 import { RegisterUseCase } from '@/use-cases/register'
 import { PrismaUsersRepository } from '@/repositories/prisma/prisma-user-repository'
+import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -21,7 +22,10 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
       password
     })
   } catch (error) {
-    return reply.status(409).send()
+    if (error instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({ message: error.message })
+    }
+    return reply.status(500).send()
   }
 
   return reply.status(201).send()
