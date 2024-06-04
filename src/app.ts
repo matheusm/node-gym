8153092717d3076d5389722983f0c92ed8d67ede
Matheus Martins <matheusms280100@gmail.com>
2024-06-03 21:21:08 -0300

    Add error handling and logging in app.setErrorHandler()

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,22 @@
 import fastify from 'fastify'
 import { appRoutes } from './routes'
+import { ZodError } from 'zod'
+import { env } from './env'
 
 export const app = fastify()
 
 app.register(appRoutes)
+
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    reply.status(400).send({ message: 'Validation error', issues: error.format() })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  } else {
+    // Log error to a service like Sentry/DataDog
+  }
+
+  return reply.status(500).send({ message: 'Internal server error' })
+})
--- src/http/register.ts
+++ src/http/register.ts
@@ -25,7 +25,8 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
     if (error instanceof UserAlreadyExistsError) {
       return reply.status(409).send({ message: error.message })
     }
-    return reply.status(500).send()
+
+    throw error
   }
 
   return reply.status(201).send()
